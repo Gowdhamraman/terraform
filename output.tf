12f447b293ea635ac5ba34a3c1213c924fc145bd
Gowdhamraman <159995194+Gowdhamraman@users.noreply.github.com>
2024-08-15 11:17:38 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "instance_ids" {
   description = "The IDs of the instances"
   value = [
-    aws_instance.web_us_east_1.id,
-    aws_instance.web_us_west_1.id
+    aws_instance.us_east_1.id,
+    aws_instance.us_west_2.id
   ]
 }
 
 output "public_ips" {
   description = "The public IP addresses of the instances"
   value = [
-    aws_instance.web_us_east_1.public_ip,
-    aws_instance.web_us_west_1.public_ip
+    aws_instance.us_east_1.public_ip,
+    aws_instance.us_west_2.public_ip
   ]
 }
