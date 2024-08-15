22973f530b2a0c77ef53c7b726fb26b2601226bd
Gowdhamraman <159995194+Gowdhamraman@users.noreply.github.com>
2024-08-15 11:18:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
-variable "ami" {
-  description = "The AMI to use for the instances"
-  type        = string
+variable "ami_us_east_1" {
+  description = "AMI for us-east-1"
+ }
+
+variable "ami_us_west_2" {
+  description = "AMI for us-west-2"
 }
 
 variable "instance_type" {
