0338869a176b82da1f3d78d422574188a1325e74
Gowdhamraman <159995194+Gowdhamraman@users.noreply.github.com>
2024-08-15 11:17:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
-resource "aws_instance" "ec2_east" {
-    provider = aws.web_us_east_1
-    ami   = var.ami
+resource "aws_instance" "us_east_1" {
+    provider = aws.us_east_1
+    ami   = var.ami_us_east_1
   instance_type = var.instance_type
+  tags = {
+    Name = "Server_us_east_1"
+  }
   }
 
-resource "aws_instance" "web_us_west_1" {
-  provider = aws.us-west-1
-  ami           = var.ami
+resource "aws_instance" "us_west_2" {
+  provider = aws.us_west_2
+  ami           = var.ami_us_west_2
   instance_type = var.instance_type
+  tags = {
+    Name = "Server_us_west_2"
+  }
 }
-
-
