a90576f529390c29d7ab561f94670051b6540494
Gowdhamraman <159995194+Gowdhamraman@users.noreply.github.com>
2024-08-15 11:17:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,20 @@
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
+terraform {
+required_providers {
+aws = {
+source  = "hashicorp/aws"
+version = "5.61.0"
+}
+}
 }
 
+# Configure the AWS Provider for us-east-1
 provider "aws" {
-  alias  = "us-west-1"
-  region = "us-west-1"
+alias  = "us_east_1"
+region = "us-east-1"
+}
+
+# Configure the AWS Provider for us-west-2
+provider "aws" {
+alias  = "us_west_2"
+region = "us-west-2"
 }
